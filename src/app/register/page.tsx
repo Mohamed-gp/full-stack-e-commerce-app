d680b17b2c2260c2b6e6071c784fa9efbc0e19c2
Mohamed-gp <mohamedterba6@gmail.com>
2024-02-16 23:55:32 +0100

    complete account page

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,46 +1,47 @@
 import Image from "next/image";
+import Link from "next/link";
+import { FaEyeSlash } from "react-icons/fa6";
 
 export default function page() {
   return (
     <>
       <div className="flex">
         <div
-          className="h-full w-1/2"
           style={{
             minHeight: "calc(100vh - 70.94px)",
-            backgroundImage: `url(/${"register-bg.png"})`,
-            backgroundRepeat: "no-repeat",
-          }}
-        ></div>
-        <div
-          style={{
-            minHeight: "calc(100vh - 70.94px)",
+            boxShadow: "rgb(255 255 255 / 30%) 0px 0px 74px 55px",
           }}
           className="flex h-full w-1/2 flex-col  justify-center px-6"
         >
           <p className="text-xl font-bold">Sign Up</p>
-          <p>Sign up for free to access to in any of our products </p>
-          <button className="my-2 flex w-full justify-center gap-2 rounded-xl   border-2 border-mainColor py-2 text-mainColor">
+          <p className="text-sm">
+            Sign up for free to access to in any of our products{" "}
+          </p>
+          <button className="my-2 flex w-full justify-center gap-2 rounded-xl   border-2 py-2 text-mainColor">
             <Image src="/Google.svg" alt="google" width={20} height={20} />
             <p>Continue With Google</p>
           </button>
-          <button className="my-2 flex w-full justify-center gap-2 rounded-xl   border-2 border-mainColor py-2 text-mainColor">
-            <Image src="/Google.svg" alt="google" width={20} height={20} />
-            <p>Continue With Google</p>
-          </button>
-          <button>Continue With Google</button>
-          <form action="" className="flex flex-col ">
+          <div className="or-sign-up relative my-2 text-center  ">
+            <span className="relative z-20 mx-auto  bg-white px-2 font-bold">
+              OR
+            </span>
+          </div>
+          <form action="" className="flex flex-col">
+            <label htmlFor="username">Username : </label>
+            <input
+              type="text"
+              id="username"
+              className="mb-2 mt-1 rounded-lg border-2 py-1 pl-2 focus:outline-none"
+            />
+
             <label htmlFor="email">Email : </label>
             <input
               type="email"
               id="email"
-              className="mt-1 rounded-lg border-2 py-2 pl-2 focus:outline-none"
+              className="mb-2 mt-1 rounded-lg border-2 py-1 pl-2 focus:outline-none"
             />
-            <label htmlFor="email" className="mb-4 mt-1 text-bgColorDanger">
-              Error
-            </label>
             <div className="flex justify-between">
-              <label htmlFor="email" className="text-bgColorDanger">
+              <label htmlFor="email" className="">
                 Password
               </label>
               <div className="mr-2 flex cursor-pointer gap-2 text-lg opacity-60">
@@ -48,18 +49,34 @@ export default function page() {
               </div>
             </div>
             <input
-              type="email"
-              id="email"
-              className="mt-1 rounded-lg border-2 py-2 pl-2 focus:outline-none"
+              type="password"
+              id="password"
+              className="mb-2 mt-1 rounded-lg border-2 py-2 pl-2 focus:outline-none"
             />
-            <label htmlFor="email" className="mb-4 mt-1 text-bgColorDanger">
+            <span className="mb-2 mt-1 text-center text-bgColorDanger opacity-50">
               Use 8 or more characters with a mix of letters, numbers & symbols
-            </label>
-            <button className="mx-auto w-fit bg-mainColor px-3 py-2 text-xl font-bold text-white">
+            </span>
+            <button className="mx-auto w-fit rounded-xl bg-mainColor px-6 py-2 text-xl font-bold text-white">
               Sign Up
             </button>
+            <div className="mt-2 flex items-center justify-center gap-2">
+              <p className="opacity-50">Already Have An Account ? </p>
+              <Link href="/login" className="text-mainColor underline">
+                Sign Up
+              </Link>
+            </div>
           </form>
         </div>
+        <div
+          className="h-full w-1/2"
+          style={{
+            minHeight: "calc(100vh - 70.94px)",
+            backgroundImage: `url(/${"purple-wallpaper-with-swirly-background.jpg"})`,
+            backgroundRepeat: "no-repeat",
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+          }}
+        ></div>
       </div>
     </>
   );
